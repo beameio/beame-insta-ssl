6071a1284df0e9fe46efca351c2840a48082ca9c
Ilya Sher <ilya.sher@coding-knight.com>
2016-09-27 20:32:49 +0300

    WIP: tests and fixes

--- testHttpServer.js
+++ testHttpServer.js
@@ -2,13 +2,10 @@
 
 const http = require('http');
 
-const b = require("beame-sdk");
-const cred = (new b.BeameStore()).getCredential(process.argv[2]);
-
-const srv = http.createServer((req, res) =>{
+const srv = http.createServer((req, res) => {
 	console.error('testHttpServer.js - request');
 	res.writeHead(200, {'Content-Type': 'text/plain', 'Server': 'Beame.io test server'});
-	res.end(process.argv[3]);
+	res.end(process.argv[2]);
 });
 
 console.log('testHttpServer - Listening on 65500');
