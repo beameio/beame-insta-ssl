1e7d1ec88c2b617c64fc93acd7632532cc8af0a0
Ilya Sher <ilya.sher@coding-knight.com>
2016-09-28 14:26:04 +0300

    Add host header test + misc

--- testHttpServer.js
+++ testHttpServer.js
@@ -5,7 +5,7 @@ const http = require('http');
 const srv = http.createServer((req, res) => {
 	console.error('testHttpServer.js - request host=%j', req.headers.host);
 	res.writeHead(200, {'Content-Type': 'text/plain', 'Server': 'Beame.io test server'});
-	res.end(process.argv[2]);
+	res.end(process.argv[2] + '-' + req.headers.host);
 });
 
 console.log('testHttpServer - Listening on 65500');
