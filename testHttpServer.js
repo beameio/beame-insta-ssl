d663c2be3161290f2685d95c54b9bb82174115ba
Ilya Sher <ilya.sher@coding-knight.com>
2016-09-28 12:27:01 +0300

    WIP: tests

--- testHttpServer.js
+++ testHttpServer.js
@@ -3,7 +3,7 @@
 const http = require('http');
 
 const srv = http.createServer((req, res) => {
-	console.error('testHttpServer.js - request');
+	console.error('testHttpServer.js - request host=%j', req.headers.host);
 	res.writeHead(200, {'Content-Type': 'text/plain', 'Server': 'Beame.io test server'});
 	res.end(process.argv[2]);
 });
